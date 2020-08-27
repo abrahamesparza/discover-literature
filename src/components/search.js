65a61656d41d9a9db9f3ca3e481dd4c8787c00e6
Abraham Esparza <abrahamesparza.us@gmail.com>
2020-08-26 22:35:04 -0700

    update component styling

--- src/components/search.js
+++ src/components/search.js
@@ -4,10 +4,8 @@ const Search = (props) => {
   return (
     <div className='tc'>
       <form className='tc'>
-        <input type='text' className='fl w-60 pa2 tc ba b--near-white' placeholder='Search a Book or an Author' onChange={props.change}/>
+        <input type='text' className='fl w-80 pa2 tc ba b--near-white' placeholder='Search a keyword to discover new books' onChange={props.change}/>
         <button type='button' className="fl w-20 pa2 bg-light-gray tc" onClick={props.click}>Submit</button>
-        <select className="tc fl w-20 pa2 bg-light-gray">
-        </select>
       </form>
     </div>
   )
